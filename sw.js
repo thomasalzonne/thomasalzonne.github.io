2e6ee1e16a388e9c1b52b02a41ed7eeb18b8c2fb
thomasalzonne <thomas.alzonne@gmail.com>
2023-01-05 10:08:44 +0100

    zeglezp

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+  };
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
       addResourcesToCache([
